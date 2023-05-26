58ce006f69042cd7e10b65c60150ca53f748884d
Alfred Broderick <alfredbr@meritagegroup.com>
2023-05-25 22:36:15 -0400

    cleanup

--- simple-queue/shared/Config.cs
+++ simple-queue/shared/Config.cs
@@ -3,6 +3,6 @@
 public static class Config
 {
     public static string ConnectionString => "<your-azure-connection-string>";
-    public static string QueueName => "<your-azure-queue-name>";
+    public static string QueueName => "test-queue";
 
 }
--- topic-queue/shared/Config.cs
+++ topic-queue/shared/Config.cs
@@ -2,7 +2,7 @@
 
 public static class Config
 {
-    public static string ConnectionString => "Endpoint=sb://meritagedevtest.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=E109aTJR7Q+AA4M17L7mUHhxWNuJGHn/x+ASbDKkaH8=";
+    public static string ConnectionString => "<your-azure-connection-string>";
     public static string QueueName => "test-queue";
     public static string TopicName => "test-topic";
     public static string[] SubscriptionName => new string[] { "even", "odd" };
